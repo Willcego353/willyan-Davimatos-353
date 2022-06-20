33898189cddad8063e8ede3a536a02295a15d19d
Willyan e Davi M. 353 <106968655+Willcego353@users.noreply.github.com>
2022-06-20 12:23:57 -0300

    Add files via upload

---
+++ atividade1pag26.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class atividade1pag26{
+
+    public static void main  (String[] args){
+    Scanner entrada = new Scanner(System.in);
+
+    Double altura, menor, maior, area;
+    System.out.println("informe a altura do trapézio em metros:");
+    altura = entrada.nextDouble();
+    System.out.println("informe a base menor do trapézio em metros:");
+    menor = entrada.nextDouble();
+    System.out.println("informe a base maior do trapézio em metros:");
+    maior = entrada.nextDouble();
+    area = (altura * (menor + maior)) / 2;
+    System.out.println("A área do trapézio é :" + area + " metros " );
+    entrada.close();
+
+}
+
+}
